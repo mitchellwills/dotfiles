d47bea39c4762732231a351133e709bb25fee15b
Mitchell Wills <mwills@wpi.edu>
2014-03-24 01:21:15 -0400

    Added confirmation for path and confirmation for install step

--- setup_user.sh
+++ setup_user.sh
@@ -1,13 +1,41 @@
 #!/bin/sh
 
 
-echo -n 'Enter path to config repo: '
-read config_path
-config_path=`readlink -f $config_path`
+config_path=`pwd`
+echo $config_path
+read -p "Use this as config directory (y/n)?" choice
+case "$choice" in 
+  y|Y ) continue;;
+  n|N ) exit 1;;
+  * ) exit 1;;
+esac
 
-set -x #echo on
+unset install
+read -p "Install stuff (y/n)?" choice
+case "$choice" in 
+  y|Y ) continue;;
+  n|N ) install="no";;
+  * ) exit 1;;
+esac
+
+(#subshell
+    echo "Creating links"
+    set -x #echo on
 
 # Configure links
-ln -bf --symbolic ${config_path}/.bashrc ~/.bashrc
-ln -bf --symbolic ${config_path}/.emacs ~/.emacs
-ln -bf --symbolic ${config_path}/.gitconfig ~/.gitconfig
+    ln -bf --symbolic ${config_path}/.bashrc ~/.bashrc
+    ln -bf --symbolic ${config_path}/.emacs ~/.emacs
+    ln -bf --symbolic ${config_path}/.gitconfig ~/.gitconfig
+)
+
+if [ -z $install ]
+then
+    (
+	echo "Installing stuff"
+	set -x #echo on
+	sudo apt-get install sl
+    )
+fi
+
+
+unset install config_path
