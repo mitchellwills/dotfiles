d5cb08adb5366512271c838bfc019a61b612e25a
Mitchell Wills <mwills@wpi.edu>
2014-05-06 20:36:09 -0400

    removed commented out stuff from install script

--- install.sh
+++ install.sh
@@ -2,14 +2,6 @@
 
 ROOT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-#unset install
-#read -p "Install stuff (y/n)?" choice
-#case "$choice" in 
-#  y|Y ) continue;;
-#  n|N ) install="no";;
-#    * ) exit 1;;
-#esac
-
 BUILD_DIR=$ROOT_PATH/build
 
 rm -fr $BUILD_DIR
@@ -34,27 +26,3 @@ for file in `find $BUILD_DIR -type f -name '*.symlink'`; do
 	fi
 done
 
-
-#echo "Creating links"
-# Configure links
-#for dir in *; do
-#	if [ -f $dir/$dir.symlink ]; then
-#		(		
-#			set -x #echo on
-#			ln -bf --symbolic $ROOT_PATH/$dir/$dir.symlink ~/.$dir;
-#		)
-#	fi
-#done
-
-
-#if [ -z $install ]
-#then
-#    (
-#	echo "Installing stuff"
-#	set -x #echo on
-#	sudo apt-get install sl yaml-mode haskell-mode
-#    )
-#fi
-
-
-#unset install config_path
