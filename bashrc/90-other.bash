49445b56dfa6588c81b00f3f944cd832390f1bd7
Mitchell Wills <mwills@wpi.edu>
2014-06-07 22:50:34 -0400

    Removed extra LANG export

--- bashrc/90-other.bash
+++ bashrc/90-other.bash
@@ -6,9 +6,6 @@ function myps() { ps $@ -u $USER -o pid,%cpu,%mem,bsdtime,command ; }
 function pstree() { myps f | awk '!/awk/ && $0~var' var=${1:-".*"} ; }
 
 
-export LANG=UTF8
-
-
 # enable programmable completion features (you don't need to enable
 # this, if it's already enabled in /etc/bash.bashrc and /etc/profile
 # sources /etc/bash.bashrc).
