e346c848717db19801a3c7950a442ef5bb139e66
Mitchell Wills <mwills@google.com>
2014-07-03 13:06:16 -0700

    Fixed python tabbing issue where tmux conf wasn't building

--- tmux.conf/tmux.py
+++ tmux.conf/tmux.py
@@ -27,35 +27,35 @@ class Tmux(ModuleBase):
         self.def_common('_', lambda : self.add_command(''))
         self.def_common('comment', lambda comment: self.add_command('# '+comment))
 
-        def do_config(self):
-            self.comment('set prefix key')
-            self.unbind('C-b')
-            self.set_option('prefix', config.tmux.prefix)
+    def do_config(self):
+        self.comment('set prefix key')
+        self.unbind('C-b')
+        self.set_option('prefix', self.config.tmux.prefix)
 
-            self._()
-            self.unbind('%', 'Remove default binding')
-            self.bind('|', 'split-window -h')
-            self.bind('_', 'split-window -v')
+        self._()
+        self.unbind('%', 'Remove default binding')
+        self.bind('|', 'split-window -h')
+        self.bind('_', 'split-window -v')
 
-            self._()
-            self.set_option('status-bg', 'black')
-            self.set_option('status-fg', 'white')
+        self._()
+        self.set_option('status-bg', 'black')
+        self.set_option('status-fg', 'white')
 
-            self._()
-            self.set_option('base-index', '1')
+        self._()
+        self.set_option('base-index', '1')
 
-            self._()
-            self.comment('Highlight active window')
-            self.set_window_option('window-status-current-bg', 'red')
+        self._()
+        self.comment('Highlight active window')
+        self.set_window_option('window-status-current-bg', 'red')
 
-            self._()
-            self.set_window_option('monitor-activity', 'on')
-            self.set_option('visual-activity', 'on')
+        self._()
+        self.set_window_option('monitor-activity', 'on')
+        self.set_option('visual-activity', 'on')
 
-        def do_build(self):
-            with open(self.build_file('.tmux.conf'), 'a') as f:
-                for c in self.commands:
-                    f.write(c+'\n')
+    def do_build(self):
+        with open(self.build_file('.tmux.conf'), 'a') as f:
+            for c in self.commands:
+                f.write(c+'\n')
 
-        def do_install(self):
-            install_symlink_in_home('.tmux.conf', self.build_file('.tmux.conf'))
+    def do_install(self):
+        install_symlink_in_home('.tmux.conf', self.build_file('.tmux.conf'))
