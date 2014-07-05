2828ff1c9a307b2b8c6aba1da176d606dae0c3a8
Mitchell Wills <mwills@google.com>
2014-07-05 15:56:34 -0700

    Added better message for untracked branches in prompt

--- bashrc/85-prompt/60-git.bash
+++ bashrc/85-prompt/60-git.bash
@@ -4,11 +4,6 @@ function git_prompt_vars {
   local ref=$(git symbolic-ref HEAD 2> /dev/null)
   SCM_BRANCH=${ref#refs/heads/}
   SCM_CHANGE=$(git rev-parse --short HEAD 2>/dev/null)
-  if [ -z $SCM_BRANCH ]; then
-	SCM_HEAD=$SCM_CHANGE
-  else
-	SCM_HEAD="$SCM_BRANCH${bash_prompt_normal}:${bash_prompt_purple}$SCM_CHANGE"
-  fi
 
   SCM_GIT_AHEAD=''
   SCM_GIT_BEHIND=''
@@ -26,12 +21,21 @@ function git_prompt_vars {
   SCM_GIT_STAGED_COUNT="$(tail -n +2 <<< "${status}" | grep -v ^[[:space:]?]  | wc -l | tr -d ' ')"
   SCM_GIT_UNSTAGED_COUNT="$(tail -n +2 <<< "${status}" | grep ^.[^[:space:]?]  | wc -l | tr -d ' ')"
   SCM_GIT_UNTRACKED_COUNT="$(tail -n +2 <<< "${status}" | grep ^??  | wc -l | tr -d ' ')"
+
+
+  if [ -z $SCM_BRANCH ]; then
+	SCM_HEAD="${bash_prompt_green}$SCM_CHANGE"
+  else
+	SCM_HEAD="${bash_prompt_green}$SCM_BRANCH"
+	[[ -z $SCM_GIT_UPSTREAM_REMOTE ]] && SCM_HEAD="$SCM_HEAD${bash_prompt_cyan}(untracked)"
+	SCM_HEAD="$SCM_HEAD${bash_prompt_normal}:${bash_prompt_purple}$SCM_CHANGE"
+  fi
 }
 
 function scm {
   if which git &> /dev/null && [[ -n "$(git rev-parse HEAD 2> /dev/null)" ]]; then
 	git_prompt_vars
-	SCM="${bash_prompt_green} |${bash_prompt_green}$SCM_HEAD"
+	SCM="${bash_prompt_green} |$SCM_HEAD"
 	if [[ $SCM_GIT_STAGED_COUNT -gt 0 || $SCM_GIT_UNSTAGED_COUNT -gt 0 || $SCM_GIT_UNTRACKED_COUNT -gt 0 ]]; then
 		SCM="$SCM ${bash_prompt_red}("
 		[[ $SCM_GIT_STAGED_COUNT -gt 0 ]] && SCM="$SCM${bash_prompt_green}+"
@@ -44,7 +48,6 @@ function scm {
 	[[ $SCM_GIT_BEHIND -gt 0 ]] && SCM=" $SCM ${bash_prompt_red}$DOWN_ARROW_SYMBOL$SCM_GIT_BEHIND"
 	[[ $SCM_GIT_AHEAD -gt 0 && $SCM_GIT_BEHIND -eq 0 ]] && SCM="$SCM${bash_prompt_cyan}"
 	[[ $SCM_GIT_AHEAD -gt 0 ]] && SCM="$SCM $UP_ARROW_SYMBOL$SCM_GIT_AHEAD"
-	[[ -z $SCM_GIT_UPSTREAM_REMOTE ]] && SCM="$SCM ${bash_prompt_blue}$UP_ARROW_SYMBOL?"
 	[[ $SCM_GIT_STASH_COUNT -gt 0 ]] && SCM="$SCM ${bash_prompt_yellow}(stash: $SCM_GIT_STASH_COUNT)"
 	SCM="$SCM${bash_prompt_green}|"
   else SCM=""
