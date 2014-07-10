90afa4a711dde0d0eb988d037b06df24310d4f90
Mitchell Wills <mwills@google.com>
2014-07-09 21:43:41 -0700

    Added some ui settings

---
+++ uisettings/uisettings.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import
+import os
+from build_util import *
+from install_util import *
+from module_base import *
+import logger
+
+class UiSettings(ModuleBase):
+    def do_install(self):
+        if self.config.uisettings.canonical_scroll:
+            logger.log('Setting normal scroll mode')
+            os.system('gsettings set com.canonical.desktop.interface scrollbar-mode normal')
+        if self.config.uisettings.nonrecursive_search:
+            logger.log('Disabling recursive search')
+            os.system('gsettings set org.gnome.nautilus.preferences enable-interactive-search true')
