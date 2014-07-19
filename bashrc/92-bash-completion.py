e7c9ffc749ae339a66638d99c5256cba1f1e5bee
Mitchell Wills <mwills@google.com>
2014-07-18 22:48:11 -0700

    Made bash completion files come from config

--- bashrc/92-bash-completion.py
+++ bashrc/92-bash-completion.py
@@ -6,6 +6,12 @@ class BashrcCompletion(ModuleBase):
     @before('Bashrc')
     def do_build(self):
         with open(self.build_file('gen/92-bash-completion.bash'), 'w') as f:
-            for completion_dir in self.config.bash.completion:
-               f.write('for f in '+os.path.join(completion_dir, '*')+'; do source $f; done')
+            for completion_dir in self.config.bash.completion_dir:
+                f.write('for f in '+os.path.join(completion_dir, '*')+'; do\n')
+                f.write('\tsource $f\n')
+                f.write('done\n')
+            for completion_file in self.config.bash.completion_file:
+                f.write('if [ -f '+completion_file+' ]; then\n')
+                f.write('\tsource '+completion_file+'\n')
+                f.write('fi\n')
 
