a9f19be01446d85a368180c36097ae7ac7f96bc1
Mitchell Wills <mwills@google.com>
2014-07-26 13:13:32 -0700

    Made ssh host configuration more flexable (doesn't have to be as verbose) Removed some bash aliases

--- ssh/ssh.py
+++ ssh/ssh.py
@@ -4,14 +4,34 @@ from collections import OrderedDict
 from install_util import *
 from module_base import *
 
+class HostConfig(object):
+    def __init__(self, host, user, hostname, identity):
+        self.host = host
+        self.user = user
+        self.hostname = hostname
+        self.identity = identity
+
 class SshConfig(ModuleBase):
+    def parse_ssh_host(self, label, data):
+        if type(data) is str:
+            full_match = re.match('(?:(.+?)\s*>\s*)?(?:(.+?)@)?(.+)', data)
+            if full_match:
+                host = full_match.group(1)
+                if host is None:
+                    host = label
+                return HostConfig(host, full_match.group(2), full_match.group(3), None)
+            else:
+                raise Exception('Unknown ssh host format: '+data)
+        else:
+            return data
+
     def do_build(self):
         with open(self.build_file('.ssh_config'), 'w') as f:
             for identity in self.config.ssh.identities:
                 f.write('IdentityFile '+identity+'\n\n')
             if self.config.ssh.hosts is not None:
                 for hostlabel in self.config.ssh.hosts.keys():
-                    host_conf = self.config.ssh.hosts[hostlabel]
+                    host_conf = self.parse_ssh_host(hostlabel, self.config.ssh.hosts[hostlabel])
                     f.write('Host '+host_conf.host+'\n')
                     if host_conf.identity is not None:
                         f.write('\tIdentityFile '+host_conf.identity+'\n')
