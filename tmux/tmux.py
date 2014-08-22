979619d011aa7cd845bf7fa25a75909fd0fcc57b
Mitchell Edwin Wills <mwills@CCCWORK4.WPI.EDU>
2014-08-22 17:39:53 -0400

    Added missing import

--- tmux/tmux.py
+++ tmux/tmux.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 from dotfiles.package_base import *
 from dotfiles.src_package import *
 from dotfiles.util import *
+from dotfiles.actions import *
 import os
 
 @suggests('tmux_conf')
